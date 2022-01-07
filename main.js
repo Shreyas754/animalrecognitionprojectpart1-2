a8f5ddbfea2311cdf6c3c11c6226704cb22d6e4c
Shreyas754 <72546939+Shreyas754@users.noreply.github.com>
2022-01-07 15:08:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/F7rl4TOJW/model.json', modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results)
+    }
+}
